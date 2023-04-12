e907ba163b7141b54ae0b2946a4890ca55d4d399
AlanMandriaza <alan.mandriaza@gmail.com>
2023-04-12 18:00:08 +0000

    agregando endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -12,6 +12,12 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    date_of_birth = db.Column(db.Date(), unique=False, nullable=True)
+    address = db.Column(db.String(120), unique=False, nullable=True)
+    city = db.Column(db.String(80), unique=False, nullable=True)
+    country = db.Column(db.String(80), unique=False, nullable=True)
+    phone_number = db.Column(db.String(20), unique=False, nullable=True)
+    avatar = db.Column(db.String(120), unique=False, nullable=True)
     roles = db.relationship('Role', secondary='user_roles')
 
     def __repr__(self):
@@ -21,7 +27,12 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-           
+            "date_of_birth": self.date_of_birth.strftime('%Y-%m-%d') if self.date_of_birth else None,
+            "address": self.address,
+            "city": self.city,
+            "country": self.country,
+            "phone_number": self.phone_number,
+            "avatar": self.avatar
         }
 
 class Role(db.Model):
@@ -57,6 +68,7 @@ class Product(db.Model):
     image = db.Column(db.String(500), nullable=False)
     category_id = db.Column(db.Integer, ForeignKey('categories.id'))
     category = db.relationship('Category', backref=db.backref('products', lazy=True))
+    quantity = db.Column(db.Integer, nullable=False, default=0)
 
     def __repr__(self):
         return f'<Product {self.name}>'
@@ -68,8 +80,10 @@ class Product(db.Model):
             "description": self.description,
             "price": self.price,
             "image": self.image,
-            "category": self.category.name
+            "category": self.category.name,
+            "quantity": self.quantity
         }
+
 
 class PaymentItem(db.Model):
     __tablename__ = 'payment_items'
--- src/app.py
+++ src/app.py
@@ -1,9 +1,7 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
+from flask_jwt import jwt, jwt_required, current_identity
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
@@ -12,19 +10,14 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
-#from models import Person
 from api.models import db, User, Role, UserRole, Category, Product, PaymentItem, Payment, Order, Review
-
-
-
-
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# database condiguration
+# database configuration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
@@ -32,7 +25,7 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db, compare_type = True)
+MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
 # Allow CORS requests to this API
@@ -44,7 +37,7 @@ setup_admin(app)
 # add the admin
 setup_commands(app)
 
-# Add all endpoints form the API with a "api" prefix
+# Add all endpoints from the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
@@ -60,16 +53,68 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
+
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = 'index.html'
     response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0 # avoid cache memory
+    response.cache_control.max_age = 0  # avoid cache memory
     return response
 
+# Signup endpoint
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
 
-# this only runs if `$ python src/main.py` is executed
+    if not email or not password:
+        raise APIException('Email and password are required', status_code=400)
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        raise APIException('User already exists', status_code=409)
+
+    new_user = User(email=email, password=password, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        raise APIException('Could not verify', status_code=401, headers={'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(email=auth.username).first()
+
+    if not user:
+        raise APIException('Could not verify', status_code=401, headers={'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if user.password == auth.password:
+        access_token = jwt.encode({'id': user.id, 'email': user.email}, app.config['JWT_SECRET_KEY'])
+        return jsonify({'access_token': access_token.decode('UTF-8')}), 200
+
+    raise APIException('Could not verify', status_code=401, headers={'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    serialized_users = [user.serialize() for user in users]
+    return jsonify(serialized_users), 200
+
+
+
+
+
+# this only runs if `$ python app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
