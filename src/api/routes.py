85ab842dc55280e732ae688bdc63d5fd6f7a6f77
AlanMandriaza <118775114+AlanMandriaza@users.noreply.github.com>
2023-04-13 19:11:24 -0400

    Update routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,21 +1,23 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+import bcrypt
+
+from flask_jwt_extended import create_access_token, jwt_required
+
+app = Flask(__name__)
+app.config.from_envvar('ENV_FILE_LOCATION')
 
 api = Blueprint('api', __name__)
 
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+
 
 # Signup endpoint
 @api.route('/signup', methods=['POST'])
@@ -39,38 +41,59 @@ def signup():
     if existing_user:
         raise APIException('User already exists', status_code=409)
 
-    new_user = User(email=email, password=password, is_active=True, first_name=first_name, last_name=last_name, date_of_birth=date_of_birth, address=address, city=city, country=country, phone_number=phone_number, avatar=avatar)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    new_user = User(email=email, password=hashed_password, is_active=True, first_name=first_name, last_name=last_name,
+                    date_of_birth=date_of_birth, address=address, city=city, country=country, phone_number=phone_number, avatar=avatar)
     db.session.add(new_user)
     db.session.commit()
 
     return jsonify({'message': 'User created successfully'}), 201
 
 
-
-
 @api.route('/login', methods=['POST'])
 def login():
-    auth = request.authorization
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
 
-    if not auth or not auth.username or not auth.password:
-        raise APIException('Could not verify', status_code=401)
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
 
-    user = User.query.filter_by(email=auth.username).first()
+    if not email:
+        return jsonify({"msg": "Missing email parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
 
-    if not user or not bcrypt.check_password_hash(user.password, auth.password):
-        raise APIException('Could not verify', status_code=401, headers={'WWW-Authenticate': 'Basic realm="Login required!"'})
+    user = User.query.filter_by(email=email).first()
+    if user is None or not bcrypt.checkpw(password.encode('utf-8'), user.password):
+        return jsonify({"msg": "Bad email or password"}), 401
 
-    if not user.is_active:
-        raise APIException('User is inactive', status_code=401)
-
-    access_token = jwt.encode({'id': user.id, 'email': user.email}, app.config['JWT_SECRET_KEY'])
-    return jsonify({'access_token': access_token.decode('UTF-8')}), 200
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token), 200
 
 
 @api.route('/users', methods=['GET'])
+@jwt_required()
 def get_users():
     users = User.query.all()
     serialized_users = [user.serialize() for user in users]
     return jsonify(serialized_users), 200
 
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    return 'API is working'
+
+
+@app.route('/sitemap.xml', methods=['GET'])
+def sitemap():
+    return generate_sitemap(app)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
