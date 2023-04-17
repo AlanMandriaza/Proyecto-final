e55067b76742ceb9fae943bc7bafc6e9afef04a1
Josefa Miller <121449537+JosefaMiller@users.noreply.github.com>
2023-04-17 21:29:18 +0000

    cart

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,6 +4,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
+import { Cart } from "./pages/cart";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
@@ -28,6 +29,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Cart />} path="/cart" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
