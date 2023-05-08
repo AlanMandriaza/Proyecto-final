3608550246456286ddf3ded77d890ed6552fa741
Yurbanis <yurbanisbriceno@gmail.com>
2023-05-08 22:34:46 +0000

    hello

--- src/front/js/pages/navbar.js
+++ src/front/js/pages/navbar.js
@@ -190,7 +190,7 @@ const MainNavbar = (props) => {
         </div>
         {localStorage.getItem("user") && (
           <p className="bienvenido">
-            Bienvenido, {localStorage.getItem("user")}
+            Bienvenido, hello {localStorage.getItem("user")}
           </p>
         )}
       </ul>
